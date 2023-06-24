e21b781c87dda1cc67df0adbbb19f22ffd3390a6
TwinsV <95010189+TwinsV@users.noreply.github.com>
2023-06-25 00:19:28 +0700

    Mengoperasikan OpenAI

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
-
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 
 
 Future<void> main() async{
@@ -39,11 +41,15 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   static const List<String> _days = ['1','3','5','7'];
-  TextEditingController _inputController = TextEditingController();
+  final TextEditingController _inputController = TextEditingController();
+  List<FileSystemEntity> file = [];
   FocusNode _inputFocus = FocusNode();
   bool _showMenu = true;
   String _selectedDay = '1';
-  
+  var jadwal = {};
+
+
+
   createSchedule() async{
   final response = await http.post(
       Uri.parse('https://api.openai.com/v1/completions'),
@@ -54,7 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: jsonEncode(
         {
           "model": "text-davinci-003",
-          "prompt": "buatkan saya jadwal " + _inputController.text + " dalam " + _selectedDay + " hari maksimal 5 tugas dalam sehari dalam format dictionary key hari ke n dan list materi secara lengkap dengan tanda double quote untuk string",
+          "prompt": "buatkan saya jadwal " + _inputController.text + " dalam " + _selectedDay + " hari maksimal 5 tugas dalam sehari dalam format dictionary key hari ke n dan list materi secara lengkap dengan tanda double quote untuk string tugas maksimal 5 kata",
           "max_tokens": 1000,
           "temperature": 0,
           "top_p": 1,
@@ -62,20 +68,79 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
     
+    var _responseModel = jsonDecode(response.body);
+    var responseTxt = _responseModel['choices'][0]['text'];
+    var jadwal_temp = json.decode(responseTxt.toString());
+    if(jadwal_temp.length > int.parse(_selectedDay)){
+      jadwal = {};
+      for (int i = 0; i < int.parse(_selectedDay); i++){
+        var key = jadwal_temp.keys.elementAt(i);
+        var value = jadwal_temp.values.elementAt(i);
+        jadwal.addAll({
+          key:value
+        }
+          );
+      }
+    }
+    debugPrint(jadwal.toString());
+    debugPrint(jadwal.length.toString());
     setState(() {
-      var _responseModel = jsonDecode(response.body);
-      var responseTxt = _responseModel['choices'][0]['text'];
-      Map<String, dynamic> jadwal = json.decode(responseTxt.toString());
-      debugPrint(responseTxt);
-      print('jadwal = ${jadwal.toString()}');
-      print(jadwal.length);
+      jadwal;
+      _inputController.text = '';
+      _showMenu = false;
     });
   }
-  
+   _read(String fileName) async {
+    try {
+      final Directory directory = await getApplicationDocumentsDirectory();
+      final Directory dir = Directory('${directory.path}/data_list/');
+      file = dir.listSync().toList();
+      if (file.isEmpty){
+        var response =  await rootBundle.loadString('assets/example.json');
+        jadwal = jsonDecode(response);
+      }else{
+        try {
+          final File file = File('${directory.path}/data_list/' + fileName + '.json');
+          var response = await file.readAsString();
+          jadwal = jsonDecode(response);
+        } catch (e) {
+          return 'error';
+        }
+      }  
+      
+    } catch (e) {
+      var response =  await rootBundle.loadString('assets/example.json');
+      jadwal = jsonDecode(response);
+      
+    }
+    
+  }
+
+  _write(String text, String fileName) async {
+    final Directory directory = await getApplicationDocumentsDirectory();
+    bool check = File('${directory.path}/data_list/$fileName.json').existsSync();
+    if(check){
+      final File file = File('${directory.path}/data_list/$fileName.json');
+      await file.writeAsString(text);
+    }else {
+      File('${directory.path}/data_list/$fileName.json').create(recursive: true);
+      Directory('${directory.path}/data_list/').create(recursive: true);
+      final File file = File('${directory.path}/data_list/$fileName.json');
+      await file.writeAsString(text);
+    }
+  }
+
   @protected
   void initState() {
     super.initState();
-    
+    setState(() {
+      _read('jadwal').then((d){
+        debugPrint('SCHEDULE = '+jadwal.toString());
+        setState(() {
+          jadwal;
+        });
+      });
+    });
   }
   @override
   Widget build(BuildContext context) {
@@ -96,7 +161,10 @@ class _MyHomePageState extends State<MyHomePage> {
                 color: Colors.red.shade100,
                 child: ListView(
                   scrollDirection: Axis.vertical,
-                  children: List.generate(7, (index) => Container(
+                  children: List.generate(jadwal.length, (index) {
+                    final title = jadwal.keys.elementAt(index);
+                    final values = jadwal.values.elementAt(index);
+                    return Container(
                     alignment: Alignment.topLeft,
                     margin: const EdgeInsets.all(10),
                     height: 150,
@@ -106,19 +174,24 @@ class _MyHomePageState extends State<MyHomePage> {
                       children: [
                         Align(
                           alignment: Alignment.topCenter,
-                          child: Text('\nTitle\n', style: TextStyle(fontSize: 16),)),
-                        Align(
-                          alignment: Alignment.topLeft,
-                          child: Column(
-                            children: List.generate(5, ((index) {
-                            return Text('data',);
-                          })),
+                          child: Container(
+                            margin: EdgeInsets.all(8),
+                            child: Text('${title}', style: TextStyle(fontSize: 18),))),
+                        Column(
+                          children: List.generate(values.length, ((index2) {
+                          final number = (index2+1).toString();
+                          return Align(
+                            alignment: Alignment.topLeft,
+                            child: Text('  $number. ${values[index2]}',
+                            ),
+                          );
+                        })),
                       
-                          ),
                         )
                       ],
                     ),
-                  )),
+                  );
+                  }),
                 ),
               )
               ),
@@ -131,18 +204,18 @@ class _MyHomePageState extends State<MyHomePage> {
                   children: [
                     Row(
                       children: [
-                        Text('Buat Jadwal '),
+                        const Text('Buat Jadwal '),
                         Container(
                           height: ((maxH/15)*1) - 5,
                           width: maxW-175,
-                          margin: EdgeInsets.only(top: 10, left:5),
+                          margin: const EdgeInsets.only(top: 10, left:5),
                           child: TextFormField(
                             controller: _inputController,
                             focusNode: _inputFocus,
                             textAlign: TextAlign.center,
-                            decoration: InputDecoration(
+                            decoration: const InputDecoration(
                               hintText: "example belajar HTML",
-                              border: OutlineInputBorder()
+                              border: const OutlineInputBorder()
                              ),
                              onEditingComplete: ((){
                               setState(() {
@@ -173,29 +246,25 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
 
 
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: List.generate(2, ((index) {
-                      return Container(
-                        width: maxW/3,
-                        height: ((maxH/15)*1) - 5,
-                        child: ElevatedButton(
-                          onPressed: (() {
-                            if(index == 1){
-                              if(_inputController.text.isNotEmpty){
-                                createSchedule();
-                              }else{
-                                debugPrint('kosong bos');
-                              }
-                            }
-                          }),
-                          child: Text(
-                            index == 0 ? 'Info' : 'Generate'  
-                          )),
-                      );
-                    }))
-                    ,)
-
+                  Center(
+                    child: Container(
+                          width: maxW/3,
+                          height: ((maxH/15)*1) - 5,
+                          child: ElevatedButton(
+                            onPressed: (() {
+                              
+                                if(_inputController.text.isNotEmpty){
+                                  createSchedule();
+                                }else{
+                                  debugPrint('kosong bos');
+                                }
+                              
+                            }),
+                            child: Text(
+                              'Generate'  
+                            )),
+                        ),
+                  ),
                   ]
                   ),
               )
