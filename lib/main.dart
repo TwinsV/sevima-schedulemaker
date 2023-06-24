89fd6771cea706acd0e0f6655a46edcd4c28a229
TwinsV <95010189+TwinsV@users.noreply.github.com>
2023-06-24 23:05:25 +0700

    menghubungkan openai
    Menghubungkan dengan OpenAI

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
@@ -33,12 +35,43 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+
+
 class _MyHomePageState extends State<MyHomePage> {
   static const List<String> _days = ['1','3','5','7'];
   TextEditingController _inputController = TextEditingController();
   FocusNode _inputFocus = FocusNode();
   bool _showMenu = true;
   String _selectedDay = '1';
+  
+  createSchedule() async{
+  final response = await http.post(
+      Uri.parse('https://api.openai.com/v1/completions'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ${dotenv.env['token']}'
+      },
+      body: jsonEncode(
+        {
+          "model": "text-davinci-003",
+          "prompt": "buatkan saya jadwal " + _inputController.text + " dalam " + _selectedDay + " hari maksimal 5 tugas dalam sehari dalam format dictionary key hari ke n dan list materi secara lengkap dengan tanda double quote untuk string",
+          "max_tokens": 1000,
+          "temperature": 0,
+          "top_p": 1,
+        },
+      ),
+    );
+    
+    setState(() {
+      var _responseModel = jsonDecode(response.body);
+      var responseTxt = _responseModel['choices'][0]['text'];
+      Map<String, dynamic> jadwal = json.decode(responseTxt.toString());
+      debugPrint(responseTxt);
+      print('jadwal = ${jadwal.toString()}');
+      print(jadwal.length);
+    });
+  }
+  
   @protected
   void initState() {
     super.initState();
@@ -59,14 +92,32 @@ class _MyHomePageState extends State<MyHomePage> {
             Expanded(
               flex: _showMenu ? 12 : 15,
               child: Container(
-                color: Colors.red,
-                child: GridView.count(
-                  crossAxisCount: 3,
+                // margin: EdgeInsets.only(top: 10),
+                color: Colors.red.shade100,
+                child: ListView(
                   scrollDirection: Axis.vertical,
-                  children: List.generate(20, (index) => Container(
-                    margin: const EdgeInsets.all(5),
-                    height: 50,
-                    color: Colors.black,
+                  children: List.generate(7, (index) => Container(
+                    alignment: Alignment.topLeft,
+                    margin: const EdgeInsets.all(10),
+                    height: 150,
+                    width: maxW - 20,
+                    color: Colors.red.shade300,
+                    child: Column(
+                      children: [
+                        Align(
+                          alignment: Alignment.topCenter,
+                          child: Text('\nTitle\n', style: TextStyle(fontSize: 16),)),
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: Column(
+                            children: List.generate(5, ((index) {
+                            return Text('data',);
+                          })),
+                      
+                          ),
+                        )
+                      ],
+                    ),
                   )),
                 ),
               )
@@ -130,6 +181,13 @@ class _MyHomePageState extends State<MyHomePage> {
                         height: ((maxH/15)*1) - 5,
                         child: ElevatedButton(
                           onPressed: (() {
+                            if(index == 1){
+                              if(_inputController.text.isNotEmpty){
+                                createSchedule();
+                              }else{
+                                debugPrint('kosong bos');
+                              }
+                            }
                           }),
                           child: Text(
                             index == 0 ? 'Info' : 'Generate'  
@@ -172,6 +230,3 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-createSchedule() async{
-  // final response = await http()
-}
